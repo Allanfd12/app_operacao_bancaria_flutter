64d6c859f585c83b095e4604e0ce70b70d320307
Allanfd12 <allanfd12@gmail.como>
2022-11-26 21:20:40 -0300

    innit commit

---
+++ android/app/src/main/kotlin/com/example/bank/application/contas_bancarias/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bank.application.contas_bancarias
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "TIVIC Bank",
+    home: Text("home"),
+      initialRoute: "/",
+      routes:{
+      }
+  ));
+}
+
